569b32cfc1569f0b2c1399fe313fa6a17cb8b4ad
jakub kapitulcin <kapitulcinjakub1@gmail.com>
2020-09-28 19:32:28 +0200

    I made function to rewrite string

--- Open-Lab-06.01/Challenge.cs
+++ Open-Lab-06.01/Challenge.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Open_Lab_06._01
 {
@@ -6,7 +7,16 @@ namespace Open_Lab_06._01
     {
         public string MysteryFunc(string str)
         {
-            throw new NotImplementedException();
+            string ret = "";
+
+            List<string> pismena = new List<string>();
+            List<int> nasobitel = new List<int>();
+
+            for (int i = 0; i < str.Length; i += 2) pismena.Add(str.ToCharArray()[i].ToString());
+            for (int i = 1; i < str.Length; i += 2) nasobitel.Add(int.Parse(str.ToCharArray()[i].ToString()));
+
+            for (int i = 0; i < pismena.Count; i++) for (int y = 0; y < nasobitel[i]; y++) ret += pismena[i];
+            return ret;
         }
     }
 }
